365e904f02731d31fcb348df0700f2ff8ee0d327
Churchill <metallic.gears.gmail.com>
2022-01-26 13:18:13 +0300

    11th

--- herokuBot.py
+++ herokuBot.py
@@ -46,6 +46,8 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 # my
+
+
 def date():
     now = datetime.datetime.now()
     return '0' + str(now.day) if len(str(now.day)) < 2 else str(now.day) + '. ' + '0' + str(now.month) if len(str(now.month)) < 2 else str(now.month)
@@ -58,42 +60,44 @@ def parse(url, tag, details):
 
 
 def corona():
-  quote = parse(
-      'https://coronavirus-monitorus.ru/moskva/',
-      'sup',
-      ''
-  )[0]
-  answ = re.findall(r'\d+', str(quote))
+    quote = parse(
+        'https://coronavirus-monitorus.ru/moskva/',
+        'sup',
+        ''
+    )[0]
+    answ = re.findall(r'\d+', str(quote))
 
-  return ' '.join(answ)
+    return ' '.join(answ)
 
 
 def bitcoin():
-  quote = parse(
-      'https://www.rbc.ru/crypto/currency/btcusd',
-      'span',
-      'currencies__td__inner'
-  )[1]
-  sum = ''.join(re.findall(r'\d+', str(quote)))
+    quote = parse(
+        'https://www.rbc.ru/crypto/currency/btcusd',
+        'span',
+        'currencies__td__inner'
+    )[1]
+    sum = ''.join(re.findall(r'\d+', str(quote)))
 
-  return sum[0:2] + ' ' + sum[2:5] + ',' + (sum[5:] if len(sum) > 6 else '00')
+    return sum[0:2] + ' ' + sum[2:5] + ',' + (sum[5:] if len(sum) > 6 else '00')
 
 
 def dollar():
-  quote = parse(
-      'https://quote.rbc.ru/ticker/72413',
-      'span',
-      'chart__info__sum'
-  )
-  sum = ''.join(re.findall(r'\d+', str(quote)))
+    quote = parse(
+        'https://quote.rbc.ru/ticker/72413',
+        'span',
+        'chart__info__sum'
+    )
+    sum = ''.join(re.findall(r'\d+', str(quote)))
 
-  return sum[0:2] + ',' + sum[2:]
+    return sum[0:2] + ',' + sum[2:]
 
 
 def message():
     return 'На *' + date() + '* количество зараженных по Москве:  *' + corona() + '*' + ' человек\n\nКурс доллара: ' + '*' + dollar() + '*' + '\u20BD\nКурс биткойна: ' + '*' + bitcoin() + '*' + '$'
 
-mes = message()
+
+mess = message()
+
 
 def main():
     """Start the bot."""
@@ -127,7 +131,7 @@ def main():
     updater.idle()
 
     updater.bot.sendMessage(
-        chat_id=chatId, text=mes)
+        chat_id=chatId, text=mess)
 
 
 if __name__ == '__main__':
