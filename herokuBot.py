eae1e16daac7a44102f95bffea7ce3d875024d7a
Churchill <metallic.gears.gmail.com>
2022-01-26 18:29:02 +0300

    13th

--- herokuBot.py
+++ herokuBot.py
@@ -18,7 +18,6 @@ logger = logging.getLogger(__name__)
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 
-
 def start(update, context):
     botMessage()
 
