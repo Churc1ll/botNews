e5bb501542b445c7b18c5e657d8f0861b50a4934
Churchill <metallic.gears.gmail.com>
2022-01-26 13:32:24 +0300

    11th

--- herokuBot.py
+++ herokuBot.py
@@ -50,8 +50,7 @@ def error(update, context):
 
 def date():
     now = datetime.datetime.now()
-    return '0' + str(now.day) if len(str(now.day)) < 2 else str(now.day) + '. ' + '0' + str(now.month) if len(str(now.month)) < 2 else str(now.month) + now.hour + now.minute
-
+    return '0' + str(now.day) if len(str(now.day)) < 2 else str(now.day) + '. ' + '0' + str(now.month) if len(str(now.month)) < 2 else str(now.month) + ' ' + now.hour 
 
 def parse(url, tag, details):
     response = requests.get(url)
