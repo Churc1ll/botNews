3f2ef6feed7f0503412d710a6c68f29b67bc1120
Churchill <metallic.gears.gmail.com>
2022-01-30 13:12:34 +0300

    13th

--- herokuBot.py
+++ herokuBot.py
@@ -63,7 +63,7 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
 
-    botMessage()
+    # botMessage()
     updater.idle()
 
 
