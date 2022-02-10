92977516be61e9aeb8be30994ed76223763667c9
Churchill <metallic.gears.gmail.com>
2022-02-10 16:57:30 +0300

    114v

--- parse.py
+++ parse.py
@@ -3,13 +3,19 @@ from bs4 import BeautifulSoup
 import re
 import datetime
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+}
+
 now = datetime.datetime.now()
+
+
 def date():
     return '*' + ('0' + str(now.day) if len(str(now.day)) < 2 else str(now.day)) + '. ' + ('0' + str(now.month) if len(str(now.month)) < 2 else str(now.month)) + '*'
 
 
 def parse(url, tag, details):
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
     answer = BeautifulSoup(response.text, 'lxml')
     return answer.find_all(tag, details)
 
@@ -31,7 +37,8 @@ def dollar():
         'col-md-2 col-xs-9 _right mono-num'
     )
     quote = quotes[0]
-    # quote = quotes[0] if now.day%2 != 0 or now.hour > 12 else quotes[1]
+    # quote = quotes[1]
+    quote = quotes[0] if now.day % 2 != 0 or now.hour > 12 else quotes[1]
     sum = ''.join(re.findall(r'\d+', str(quote)[33:]))
     return '*' + sum[0:2] + ',' + sum[2:4] + '*'
 
@@ -46,8 +53,24 @@ def bitcoin():
     return sum[0:2] + ' ' + sum[2:5] + ',' + (sum[5:] if len(sum) > 6 else '00')
 
 
-def message():
-    return 'За ' + date() + ' количество зараженных по Москве:  *' + corona() + '*' + ' человек\n\nКурс доллара: ' + dollar() + '\u20BD\nКурс биткойна: ' + '*' + bitcoin() + '*' + '$'
 
-message = message() 
+def weather(): 
+  quote = parse(
+      'https://www.gismeteo.ru/weather-moscow-4368/2-weeks/',
+      'div',
+      'widget-row-chart widget-row-chart-temperature-avg'
+  )
+  sum = ''.join(re.findall(
+      r'(?<=unit unit_temperature_c).*', str(quote)))[2:3]
+  return sum
+
+
+def message():
+    return 'За ' + date() + ' количество зараженных по Москве:  *' + corona() + '*' + ' человек\nСреднесуточная температура: ' + weather() + '\u00B0\n\nКурс доллара: ' + dollar() + '\u20BD\nКурс биткойна: ' + '*' + bitcoin() + '*' + '$'
+
+
+message = message()
 print(message)
+
+
+
